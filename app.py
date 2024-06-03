09a932cffbaaa6b71072a2ac507b931b49353c99
SoukaynaBetahi <147205539+SoukaynaBetahi@users.noreply.github.com>
2024-06-03 16:06:24 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+# Q&A Chatbot
+#from langchain.llms import OpenAI
+
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+import streamlit as st
+import os
+import pathlib
+import textwrap
+
+import google.generativeai as genai
+
+from IPython.display import display
+from IPython.display import Markdown
+
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+## Function to load OpenAI model and get respones
+
+def get_gemini_response(question):
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(question)
+    return response.text
+
+##initialize our streamlit app
+
+st.set_page_config(page_title="supmti-bot", page_icon=None)
+
+st.header("supmti-bot")
+
+input=st.text_input("Enter: ",key="input")
+
+
+submit=st.button("Poser la question")
+
+## If ask button is clicked
+
+if submit:
+    
+    response=get_gemini_response(input)
+    st.subheader("la réponse")
+    st.write(response)
